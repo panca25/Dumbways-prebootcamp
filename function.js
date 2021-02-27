f907093fee9d9e2dc3c9d9fe8b4e95b8c5dc9662
Panca Setiawan <pancasetiawan225@gmail.com>
2021-02-27 21:26:09 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+// Regular Function
+function print (input1, input2){
+    console.log(input1 +" " + input2);
+}
+print("panca", "set");
+
+// Arrow Function
+print = (input1, input2) => console.log(input1 + " " + input2);
+print("Asep", "Setiawan");
